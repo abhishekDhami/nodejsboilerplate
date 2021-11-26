e0060911c4a1537a201949e7bcb817b636e6ddb3
admin <admin@Rajat-Sethi.dppune.local>
2021-11-26 15:33:44 +0530

    ini commit 2

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,41 @@
-let a:string='hello';
-console.log('huha'+a)
+import {setTimeout} from 'timers'
+
+let pr1=new Promise((res,rej)=>{
+    setTimeout(()=>{
+        res('pr1');
+    },400)
+})
+
+let pr2=new Promise((res,rej)=>{
+    setTimeout(()=>{
+        res('pr2');
+    },5000)
+})
+
+let pr3=new Promise((res,rej)=>{
+    setTimeout(()=>{
+        rej('pr3');
+    },100)
+})
+
+//It will give output once all are successful, throw error if anyone has failed before completing other tasks
+// let p=Promise.all([pr1,pr2,pr3]).then((d)=>{
+//     console.log(d)
+// }).catch((err)=>{
+//     console.log(err)
+// })
+
+//It will give error or success for any one promise which is fastest
+// let p=Promise.race([pr1,pr2,pr3]).then((d)=>{
+//     console.log(d)
+// }).catch((err)=>{
+//     console.log('error'+err)
+// })
+
+//It will wait for all promise to complete it with resolve or reject
+// let p=Promise.allSettled([pr1,pr2,pr3]).then((d)=>{
+//     console.log(d)
+// }).catch((err)=>{
+//     console.log('error'+err)
+// })
+   
